cc980fc39eb191e9beae8d1179ed0d9c95ef8f2c
Valyn <115489989+ValynTyler@users.noreply.github.com>
2024-03-09 17:24:08 +0200

    Added orthographic projection matrix

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -224,6 +224,22 @@ impl Matrix4 {
             0.0, 0.0, 0.0, 1.0,
         ])
     }
+
+    pub fn ortho(
+        left: f32,
+        right: f32,
+        bottom: f32,
+        top: f32,
+        near: f32,
+        far: f32,
+    ) -> Matrix4 {
+        Matrix4([
+            2./(right-left),  0.,               0.,              -(right+left) / (right-left),
+            0.,               2./(top-bottom),  0.,              -(top+bottom) / (top-bottom),
+            0.,               0.,               2./(far-near),   -(far+near)   / (far-near),
+            0.,               0.,               0.,               1.,
+        ])
+    }
 }
 
 impl ops::Mul<Vector4> for Matrix4 {
