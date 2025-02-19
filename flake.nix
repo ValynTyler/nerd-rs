badea85f1792300af5b63548e6a9319807fad1a3
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-19 15:57:25 +0200

    feat: init project

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, ... } @ inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
+    let
+        overlays = [ (import inputs.rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+    in
+    {
+      devShells.default = with pkgs; mkShell {
+        buildInputs = [
+          rust-bin.nightly.latest.default
+        ];
+      };
+    }
+  );
+}
+
